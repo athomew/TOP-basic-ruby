cc618397bf9196b36dc98121d11fdbe078f58906
Aline Thome <alinethomew@gmail.com>
2017-07-10 22:28:20 -0500

    Do bubble sort exercise.

---
+++ bubble-sort.rb
@@ -0,0 +1,21 @@
+def bubble_sort array
+  swaps = true
+  while(swaps)
+    swaps = false
+    (array.length - 1).times do |i|
+      array[i], array[i+1], swaps = array[i+1], array[i], true if array[i] > array[i+1]
+    end
+  end
+  array
+end
+
+def bubble_sort_by array, &block
+  swaps = true
+  while(swaps)
+    swaps = false
+    (array.length - 1).times do |i|
+      array[i], array[i+1], swaps = array[i+1], array[i], true if (block.call(array[i], array[i+1]) == 1)
+    end
+  end
+  array
+end
